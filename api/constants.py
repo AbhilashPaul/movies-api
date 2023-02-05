38bb38610ef72bf84e62e9aa968cd7de3a57bc09
abhilashpaul <paul.abhilash@hotmail.com>
2023-02-05 12:51:50 -0700

    extract paths to constants

--- api/constants.py
+++ api/constants.py
@@ -1,5 +1,11 @@
+PATH_MOVIE = "/api/movies/<movie_id>"
+PATH_MOVIE_LIST = "/api/movies"
+PATH_MOVIE_LIKES = "/api/movies/<movie_id>/likes"
+PATH_MOVIE_DISLIKES = "/api/movies/<movie_id>/dislikes"
+
 CHARACTER_LIMIT_DESCRIPTION = 300
 CHARACTER_LIMIT_TITLE = 100
+
 PROPERTY_TITLE = "title"
 PROPERTY_DESCRIPTION = "description"
 PROPERTY_RELEASE_YEAR = "release_year"
--- app.py
+++ app.py
@@ -4,11 +4,13 @@ from flask import Flask
 from flask_alembic import Alembic
 from flask_restful import Api
 
+from api.constants import PATH_MOVIE_LIST, PATH_MOVIE, PATH_MOVIE_LIKES, PATH_MOVIE_DISLIKES
 from api.resources import MovieResource, MovieListResource, MovieLikesResource, MovieDislikesResource
 from api.models import db, Movie
 from config import configurations
 
 alembic = Alembic()
+
 
 def create_app():
     app = Flask(__name__)
@@ -16,10 +18,10 @@ def create_app():
     db.init_app(app)
     alembic.init_app(app)
     api = Api(app)
-    api.add_resource(MovieListResource, "/api/movies")
-    api.add_resource(MovieResource, "/api/movies/<movie_id>")
-    api.add_resource(MovieLikesResource, "/api/movies/<movie_id>/likes")
-    api.add_resource(MovieDislikesResource, "/api/movies/<movie_id>/dislikes")
+    api.add_resource(MovieListResource, PATH_MOVIE_LIST)
+    api.add_resource(MovieResource, PATH_MOVIE)
+    api.add_resource(MovieLikesResource, PATH_MOVIE_LIKES)
+    api.add_resource(MovieDislikesResource, PATH_MOVIE_DISLIKES)
     return app
 
 

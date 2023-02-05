54361532334ef94088edda0c17f5347f76dabd37
abhilashpaul <paul.abhilash@hotmail.com>
2023-02-05 12:31:01 -0700

    adds app.py

---
+++ app.py
@@ -0,0 +1,28 @@
+import os
+from dotenv import load_dotenv
+from flask import Flask
+from flask_alembic import Alembic
+from flask_restful import Api
+
+from api.resources import MovieResource, MovieListResource, MovieLikesResource, MovieDislikesResource
+from api.models import db, Movie
+from config import configurations
+
+
+def create_app():
+    load_dotenv()
+    app = Flask(__name__)
+    app.config.from_object(configurations.get(os.getenv('APP_CONFIG') or 'default'))
+    db.init_app(app)
+    Alembic(app)
+    api = Api(app)
+    api.add_resource(MovieListResource, "/api/movies")
+    api.add_resource(MovieResource, "/api/movies/<movie_id>")
+    api.add_resource(MovieLikesResource, "/api/movies/<movie_id>/likes")
+    api.add_resource(MovieDislikesResource, "/api/movies/<movie_id>/dislikes")
+    return app
+
+
+if __name__ == '__main__':
+    app = create_app()
+    app.run(port=5100)
---
+++ config.py
@@ -0,0 +1,29 @@
+import os
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+
+class Config(object):
+    TESTING = False
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+
+class ProductionConfig(Config):
+    SQLALCHEMY_DATABASE_URI = "sqlite:///"+os.path.join(basedir, 'prod_movies.db')
+
+
+class DevelopmentConfig(Config):
+    SQLALCHEMY_DATABASE_URI = "sqlite:///"+os.path.join(basedir, 'dev_movies.db')
+
+
+class TestingConfig(Config):
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///:memory:'
+    TESTING = True
+
+
+configurations = {
+    'dev': DevelopmentConfig,
+    'prod': ProductionConfig,
+    'default': DevelopmentConfig,
+    'test': TestingConfig
+}
